809f0f0bd033da7e0c026f4e373dd1eb97609550
DianaSenko <gruntic@mail.ru>
2025-03-31 19:46:49 +0400

    добавление vuetify в main.ts

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,13 +1,27 @@
 import { createApp } from 'vue'
+
 import './style.css'
 
 
+
+import 'vuetify/styles'
+
 import {createPinia} from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 
 const pinia = createPinia()
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
 const app = createApp(App)
 
 app.use(pinia)
+app.use(vuetify)
 
 app.mount('#app')
