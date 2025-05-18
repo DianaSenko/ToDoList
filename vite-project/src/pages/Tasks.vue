236c41b7b6100d802dfe6987d9af2bbbd8e27feb
DianaSenko <gruntic@mail.ru>
2025-05-18 21:27:37 +0400

    Правка filteredNotes (дата все еще не фильтруется)

--- vite-project/src/pages/Tasks.vue
+++ vite-project/src/pages/Tasks.vue
@@ -19,6 +19,7 @@
           <NoteList
           :notes="filteredNotes"
           @delete="deleteNote"/>
+       
         </v-list>
       </v-card-text>
     </v-card>
@@ -37,28 +38,24 @@ const search = ref("");
 const showDialog = ref(false);
 let notes = ref([]);
 
-const filteredNotes = computed(() => {
-  const query = search.value.toLowerCase();
-  return notes.value.filter(
-    (note) =>
-      note.lastname.toLowerCase().includes(query) ||
-      note.name.toLowerCase().includes(query) ||
-      note.surname.toLowerCase().includes(query) ||
-      dateRange(note).includes(query) ||
-      note.title.toLowerCase().includes(query) ||
-      note.content.toLowerCase().includes(query)
-  );
-});
-
 const refreshTask = async () => {
   const tasks = await getTasks();
-
   notes.value = tasks;
-
-  console.log(tasks);
-  console.log(notes);
-  console.log(notes.value);
+  
 };
+
+const filteredNotes = computed(() => {
+  const query = search.value.toLowerCase();
+  return notes.value.filter(note => 
+    note.lastname?.toLowerCase().includes(query) || 
+    note.name?.toLowerCase().includes(query) ||
+    note.surname?.toLowerCase().includes(query) ||
+    // note.daterange?.includes(query) ||
+    note.title?.toLowerCase().includes(query) ||
+    note.content?.toLowerCase().includes(query)
+    
+  );
+});
 
 const deleteNote = async (id) => {
   await deleteTask(id);
